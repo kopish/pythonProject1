ac9ccc1a4b0c6e2ff5f8bbaf0519d15deb5f94dd
Dell <kopish88@gmail.com>
2023-03-08 17:15:24 +0200

    Задачі 6

--- Telegram  Currency/Currensy.py
+++ Telegram  Currency/Currensy.py
@@ -1,11 +1,18 @@
 import requests
 import xml.etree.ElementTree as ET
 
+
 def convert_currency(amount, from_currency, to_currency):
     base_url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange'
-    url = f'{base_url}&symbols={from_currency},{to_currency}'
+    url = f'{base_url}&valcode={from_currency}&valcode={to_currency}&xml'
     response = requests.get(url)
-    data = response.json()
-    exchange_rate = data['rates'][to_currency] / data['rates'][from_currency]
+    root = ET.fromstring(response.content)
+    rates = {elem.find('cc').text: float(elem.find('rate').text) for elem in
+             root.findall('.//currency')}
+    exchange_rate = rates[to_currency] / rates[from_currency]
     result = round(amount * exchange_rate, 2)
     return result
+
+
+result = convert_currency(100, 'USD', 'EUR')
+print(result)
