8c2f1e2d17aa7ee7ac8cec207c96b52e319ab9ba
Dell <kopish88@gmail.com>
2023-03-10 11:53:08 +0200

    Гусак виконано ДЗ

--- Гра Гусак/main.py
+++ Гра Гусак/main.py
@@ -8,6 +8,8 @@ screen = width, height = 800, 600
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
+RAD = 255, 0, 0
+BLUE = 0, 0, 255
 
 main_surface = pygame.display.set_mode(screen)
 
@@ -26,6 +28,13 @@ while True:
     bal_rect = bal_rect.move(ball_speed)
     if bal_rect.bottom >= height or bal_rect.top <= 0:
         ball_speed[1] = -ball_speed[1]
+        ball.fill(WHITE)
+    if bal_rect.right >= width or bal_rect.right <= 0:
+        ball_speed[0] = ball_speed[1]
+        ball.fill(RAD)
+    if bal_rect.left >= width or bal_rect.left <= 0:
+        ball_speed[0] = ball_speed[1]
+        ball.fill(BLUE)
     main_surface.fill((0, 0, 0))
     main_surface.blit(ball, (bal_rect))
 
